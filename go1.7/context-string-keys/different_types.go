0bb76a0550b37695cfe80fc4b1fd23797bcf4129
Mat Ryer <thatmatryer@gmail.com>
2016-07-24 14:48:25 +0100

    added different types

---
+++ go1.7/context-string-keys/different_types.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+)
+
+type contextKeyA string
+type contextKeyB string
+
+var (
+	contextKeyOne = contextKeyA("key")
+	contextKeyTwo = contextKeyB("key")
+)
+
+func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, contextKeyOne, "one")
+	ctx = context.WithValue(ctx, contextKeyTwo, "two")
+	log.Println(" first key", ctx.Value(contextKeyOne))
+	log.Println("second key", ctx.Value(contextKeyTwo))
+}
+
+// Output:
+// 2016/07/24 14:44:40  first key one
+// 2016/07/24 14:44:40 second key two
